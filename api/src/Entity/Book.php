f72bc18043c736ee3ec221fedef22245fc15a158
Souleyman BEN HADJ BELGACEM <souleyman@MacBook-Pro-de-Souleyman.local>
2023-07-18 16:45:54 +0200

    rename short description path into titles

--- api/src/Entity/Book.php
+++ api/src/Entity/Book.php
@@ -13,7 +13,7 @@ use Doctrine\ORM\Mapping as ORM;
     operations:[
     new Get(),
     new GetCollection(),
-    new GetCollection(uriTemplate:"books-short-description"),
+    new GetCollection(uriTemplate:"books-titles"),
     new Post()
      ]
      )]
